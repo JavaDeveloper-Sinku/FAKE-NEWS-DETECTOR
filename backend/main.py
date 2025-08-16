eb2c1be120bf920a8f34d4d4c528dbb7dabf4fa4
JavaDeveloper-Sinku <sinkusingh173@gmail.com>
2025-08-16 14:59:05 +0530

    second update

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from models.model_loader import predict_fake_news
 from api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 # FastAPI app
 app = FastAPI(title="Fake News Detector API", version="1.0.0")
 
+
+#CORS steup ( frontend port )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class NewsRequest(BaseModel):
     text: str
@@ -14,6 +24,8 @@ class NewsRequest(BaseModel):
 # Routes register karna
 app.include_router(router)
 
+
+#API Endpoints
 @app.get("/")
 def home():
     return {"message": "Fake News Detector API is running"}
